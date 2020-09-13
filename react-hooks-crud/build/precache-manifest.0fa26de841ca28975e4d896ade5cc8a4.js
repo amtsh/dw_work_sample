ec29a871e3db3fc6a8c8a35d4609b057ae229b7b
sukeshni <sukeshni.kantrod@gmail.com>
2020-09-13 18:19:23 +0900

    do not ignore build folder

---
+++ react-hooks-crud/build/precache-manifest.0fa26de841ca28975e4d896ade5cc8a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed9f0a18ef2d30cb4ab592519849b95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f63f3abd4bfbe1ec02cc",
+    "url": "/static/css/2.d2f44748.chunk.css"
+  },
+  {
+    "revision": "037587088bdbc641e0b5",
+    "url": "/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "f63f3abd4bfbe1ec02cc",
+    "url": "/static/js/2.1ddaec50.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ddaec50.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "037587088bdbc641e0b5",
+    "url": "/static/js/main.bef55bf8.chunk.js"
+  },
+  {
+    "revision": "24bda4603e57f08e575b",
+    "url": "/static/js/runtime-main.ee957edc.js"
+  }
+]);
