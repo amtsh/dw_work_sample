94423eec580eb4be3cda5a0845a37aece66f161e
sukeshni <sukeshni.kantrod@gmail.com>
2020-09-13 18:20:31 +0900

    use deployed postgres db from heroku if env set

--- app/models/index.js
+++ app/models/index.js
@@ -2,18 +2,36 @@
 const dbConfig = require("../config/db.config.js");
 
 const Sequelize = require("sequelize");
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.dialect,
-  operatorsAliases: false,
+let sequelize;
 
-  pool: {
-    max: dbConfig.pool.max,
-    min: dbConfig.pool.min,
-    acquire: dbConfig.pool.acquire,
-    idle: dbConfig.pool.idle,
-  },
-});
+console.log("Value of process.env");
+console.log(process.env);
+
+if (process.env.DATABASE_URL) {
+  console.log("In process.env.DATABASE_URL");
+  console.log(process.env.DATABASE_URL);
+
+  // the application is executed on Heroku ... use the postgres database
+  sequelize = new Sequelize(process.env.DATABASE_URL, {
+    dialect: "postgres",
+    protocol: "postgres",
+    logging: true, //false
+  });
+} else {
+  console.log("else process.env.DATABASE_URL");
+  sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+    host: dbConfig.HOST,
+    dialect: dbConfig.dialect,
+    operatorsAliases: false,
+
+    pool: {
+      max: dbConfig.pool.max,
+      min: dbConfig.pool.min,
+      acquire: dbConfig.pool.acquire,
+      idle: dbConfig.pool.idle,
+    },
+  });
+}
 
 const db = {};
 
